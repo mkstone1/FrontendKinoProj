12dd7d1c0bd74de157d0a81c07ed00d606db1881
alexander6273 <alex6273@stud.kea.dk>
2022-10-12 14:23:42 +0200

    create user fix

--- pages/createUser/createUser.js
+++ pages/createUser/createUser.js
@@ -1,4 +1,4 @@
-import { handleHttpErrors, kinoUrlUsers} from "../../utils.js";
+import { handleHttpErrors, kinoUrlUsers, setErrorMessage} from "../../utils.js";
 
 export function initCreateUser(){
     window.addEventListener("load", createUser())
@@ -7,14 +7,16 @@ export function initCreateUser(){
 async function createUser(){
     document.querySelector("#btn-add-user").onclick = checkPasswordAreIdentical
 
-    function checkPasswordAreIdentical(){
+    async function checkPasswordAreIdentical(){
         const password = document.querySelector("#input-password").value
         const checkPassword = document.querySelector("#input-password-again").value
         if(password == checkPassword){
             makeNewUser()
+            console.log(password)
         }
         else{
             document.querySelector("#update-status").innerHTML = "password stemmer ikke overens"
+            console.log(password)
         }
     }
 
@@ -22,7 +24,7 @@ async function createUser(){
         const newUser = {}
         newUser.firstName = document.querySelector("#input-first-name").value
         newUser.lastName = document.querySelector("#input-last-name").value
-        newUser.userName = document.querySelector("#input-username").value
+        newUser.username = document.querySelector("#input-username").value
         newUser.email = document.querySelector("#input-email").value
         newUser.password = document.querySelector("#input-password").value
 
@@ -31,15 +33,7 @@ async function createUser(){
         options.headers = {"Content-type": "application/json"}
         options.body = JSON.stringify(newUser)
         const addUser = await fetch(kinoUrlUsers, options).then(handleHttpErrors)
-        const createStatus = document.querySelector("#update-status")
-        createStatus.style.display ="flex"
-        createStatus.style.justifyContent = "center"
-        if(addUser){
-            document.querySelector("#update-status").innerHTML = "Bruger oprettet"
-        }
-        else{
-            document.querySelector("#update-status").innerHTML = "Fejl ved oprettelse"
-        }
+        setErrorMessage(addUser, "Bruger oprettet")
     }
 }
 
